4bb189c81c471b8407fa3bbd85c40de02775dccb
Abdykali Azat <azat.abdykalli@gmail.com>
2024-09-12 16:53:27 +0500

    ecargovko client 5

---
+++ src/config.js
@@ -0,0 +1,11 @@
+// config.js
+const config = {
+    // apiUrl: 'http://localhost:3001',
+    apiUrl: 'https://e-cargovka.kz:3001',
+    
+    nameCargo: 'E CARGO VKO'
+  };
+  
+  
+export default config;
+  
